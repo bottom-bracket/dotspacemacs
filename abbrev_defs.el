55bfbcc7c2d0fae0f1e499e8084ea5947f39c313
Philip Heringlake <philip.heringlake@neel.cnrs.fr>
2020-05-11 14:31:34 +0200

    typing is learning

---
+++ abbrev_defs.el
@@ -0,0 +1,12 @@
+;;-*-coding: utf-8;-*-
+(define-abbrev-table 'org-mode-abbrev-table
+  '(
+    ("colors" "Colours" nil :count 0)
+    ("comlexity" "complexity" nil :count 0)
+    ("estat" "electrostatic" nil :count 2)
+    ("freq" "frequency" nil :count 1)
+    ("freqs" "frequencies" nil :count 2)
+    ("frq" "frequency" nil :count 0)
+    ("mistkake" "mistake" nil :count 0)
+   ))
+
